12aeee256971ffde9433cf6ce06f8596704de52c
miaras <miarab@pm.me>
2022-02-27 07:54:45 -0800

    Fix appropriate string location finder logic

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ if (window.location.href.includes("google.com/search?")) {
     let u_changed = false;
     const arr = u.split("&");
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i].includes("google.com/search?")
+        if (arr[i].includes("q=")
             && !arr[i].includes("+-quora.com")) {
             arr[i] = arr[i].concat("+-quora.com");
             u_changed = true;
